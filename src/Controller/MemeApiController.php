b30b30a1ad5f2286bc74e456f690e9fc29e707d5
Szymon Matynia <szymon@vrocode.home>
2019-05-06 02:00:18 +0200

    adding memes has been implemented - there is much code to refactor, also should write some tests!

--- src/Controller/MemeApiController.php
+++ src/Controller/MemeApiController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\MemeApi;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -15,6 +16,7 @@ class MemeApiController extends AbstractController
     private $appKernel;
     private $memeApiRepository;
     private $em;
+    private const IMAGES_PATH = '/public/uploads/';
     public function __construct(MemeApiRepository $memeApiRepository, EntityManagerInterface $em, KernelInterface $appKernel)
     {
         $this->memeApiRepository = $memeApiRepository;
@@ -43,9 +45,15 @@ class MemeApiController extends AbstractController
             $type = pathinfo($path, PATHINFO_EXTENSION);
             $data[] = ['title' => $mms->getTitle(), 'base64' => $base64, 'type' => $type];
         }
-        return $this->json([
+
+        // use only if dont find solution to unescaped slashes
+        $response = new Response(json_encode($data, JSON_UNESCAPED_SLASHES), 200, ['application/json']);
+        $response->headers->set('Content-Type', 'application/json');
+        return $response;
+
+        /*return $this->json([
             'data' =>  $data,
-        ], 200);
+        ], 200);*/
     }
 
     public function getMeme($id)
@@ -82,19 +90,47 @@ class MemeApiController extends AbstractController
 
     }
 
-    public function deleteMeme()
+    public function deleteMeme($id)
     {
-
+        $meme = $this->memeApiRepository->find($id);
+        if(!$meme)
+        {
+            return $this->json([
+                'message' => 'No such id in the database',
+            ], 404);
+        }
+        // delete the file
     }
 
     public function updateMeme()
     {
-
+        // update the title of meme only if author or admin :)
     }
 
-    public function addMeme()
+    public function addMeme(Request $request)
     {
 
+        $uploadedFile = $request->files->get('file');
+        //return new JsonResponse(var_dump($uploadedFile->getPathname()));
+        if(!isset($uploadedFile) || empty($uploadedFile)) return new JsonResponse(['message' => 'Please upload file.'], 400);
+
+        $uploadedFileType = $uploadedFile->getMimeType();
+
+
+        if(!($uploadedFileType == 'image/png' || $uploadedFileType == 'image/jpeg' || $uploadedFileType == 'image/gif'))
+        {
+            return new JsonResponse(['message' => 'Not allowed type sent.'], 400);
+        }
+
+        $memeApi = new MemeApi();
+        $memeApi->setTitle(uniqid());
+        $memeApi->setImage(MemeApiController::IMAGES_PATH . $memeApi->getTitle() . "." . $uploadedFile->getClientOriginalExtension());
+        $this->em->persist($memeApi);
+        $this->em->flush();
+
+        move_uploaded_file($uploadedFile->getPathname(), $this->appKernel->getProjectDir() . $memeApi->getImage());
+
+        return new JsonResponse(['message' => 'Image ' . $uploadedFile->getClientOriginalName() . ' has been added to db with name: ' . $memeApi->getTitle()]);
     }
 }
 
